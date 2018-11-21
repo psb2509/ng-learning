2f0dbb4fd29e32af1c3594f31ee1462c4500bc26
Pradeep Singh <pradeep.bisht0303@gmail.com>
2018-11-20 22:05:41 -0500

    Test Component to understand interpolation and property binding between ts and html of a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewformComponent } from './newform/newform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NewformComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/newform/newform.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.app-newform',
+  templateUrl: './newform.component.html',
+  styleUrls: ['./newform.component.css']
+})
+export class NewformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+public myName = "Pradeep Singh";
+
+public isDisable = false ;
+}
