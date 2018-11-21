dff7186ac0eb7d63a5670a7d76b10041173490a4
Pradeep Singh <pradeep.bisht0303@gmail.com>
2018-11-21 09:16:18 -0500

    event binding

--- src/app/newform/newform.component.ts
+++ src/app/newform/newform.component.ts
@@ -4,10 +4,6 @@ import { Component, OnInit } from '@angular/core';
   selector: '.app-newform',
   template: `
             <h2>Welcome {{myName}}</h2>
-            <h2 [style.color]="'red'">Style Binding 1</h2>
-            <h2 [style.color]="mycall ? 'Blue' : 'red'">Style Binding 2</h2> 
-            <h2 [style.color]="mycall ? mycolor : 'red'">Style Binding 3</h2> 
-            <h2 [ngStyle]="mycall ? myStyles :'' ">Style Binding 3</h2> 
             
             `,
   styles: [`
@@ -24,12 +20,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewformComponent implements OnInit {
   public myName = "Pradeep Singh";
-  public mycall = true;
-  public mycolor = 'yellow';
-  public myStyles = {
-    'color': 'red',
-    'font-style': 'italic'
-};
+  
   constructor() { }
 
   ngOnInit() {
